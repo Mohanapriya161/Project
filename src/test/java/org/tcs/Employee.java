636ea51e4a9ddab8ad1db478634c7f09357b48bc
Mohanapriya161 <vmohanapriya50gmail.com>
2021-11-12 08:46:40 +0100

    First time pushing the code

---
+++ src/test/java/org/tcs/Employee.java
@@ -0,0 +1,28 @@
+package org.tcs;
+
+import org.testng.annotations.Test;
+
+public class Employee {
+	
+	
+	@Test
+	public void test1() {
+		System.out.println("Test 1");
+
+	}
+	@Test
+	public void test2() {
+		System.out.println("Test 2");
+
+	}
+	@Test
+	public void test9() {
+		System.out.println("Test 9");
+
+	}
+	@Test
+	public void test6() {
+		System.out.println("Test 6");
+
+	}
+}
